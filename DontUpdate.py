bfafb3d7d42dd21c142d4152fdf942090bd1b8fb
ADDB <addb1234567890@gmail.com>
2023-03-22 21:20:09 +0100

    Removed need for steam account

--- DontUpdate.py
+++ DontUpdate.py
@@ -4,62 +4,31 @@ import acf
 from findSteam import findSteam
 import os
 from gevent import timeout
-from requests import ConnectionError
 
 
 steamdir = findSteam()
 print("Found Steam in: " + steamdir)
 mySteam = steam.client.SteamClient()
-username = input("Enter Username: ")
-password = input("Enter Password: ")
+mySteam.anonymous_login()
+
 appID = input("Enter AppID of Game: ").strip()
-
-tries = 0
-retry = True
-
-while retry:
-    try:
-        retry = False
-        res = mySteam.login(username=username, password=password)
-    except ConnectionError:
-        print("Connection error! Retrying...")
-        tries += 1
-        if tries < 5:
-            retry = True
-        else:
-            print("Couldn't build a connection. Aborting...")
-            quit()
-
-if res == steam.client.EResult.InvalidPassword:
-    print("Invalid password or too many login attempts!")
-    quit()
-
-
-@mySteam.on(mySteam.EVENT_AUTH_CODE_REQUIRED)
-def auth_code_prompt(is_2fa, _):
-    if is_2fa:
-        code = input("Enter 2FA (Stema Guard) Code: ")
-        if mySteam.login(username, password, two_factor_code=code) == steam.client.EResult.OK:
-            print("Login successful")
-    else:
-        code = input("Enter Email Code: ")
-        if mySteam.login(username, password, two_factor_code=code) == steam.client.EResult.OK:
-            print("Login successful")
-
-
 myCDN = CDNClient(mySteam)
+
 repeat = True
+
 while repeat:
     try:
         repeat = False
         response = myCDN.get_app_depot_info(app_id=int(appID))
+        if response is None:
+            repeat = True
     except timeout.Timeout:
         repeat = True
 
 with open(os.path.join(steamdir, f"appmanifest_{appID}.acf"), "r") as inp:
     file = acf.load(inp)
 
-print(f"Modyfing {file['AppState']['name']}")
+print(f"Modyfing for Game: {file['AppState']['name']}")
 
 file["AppState"]["StateFlags"] = 4
 for depot in file["AppState"]["InstalledDepots"].keys():
@@ -72,6 +41,12 @@ if file["AppState"].get("TargetBuildID") is not None:
         if response['branches'][branch]['buildid'] == targetBuildID:
             targetBranch = branch
             break
+
+# TODO: Seems broken for other branches?
+# To reproduce: Change to a Beta. Close Steam. -> Trying to apply program will not work?.
+# Other direction works (If you are on a beta build, switching to the public build and then using the program
+# Allows staying on beta files
+print(f"Target Branch: {targetBranch}")
 
 targetBranchBuildID = response['branches'][targetBranch]['buildid']
 file["AppState"]["buildid"] = targetBranchBuildID
